347ffab84a1cd0265392c0381e5b38245775e951
Shlok Verma <vermashlok.k@gmail.com>
2025-03-22 13:30:57 +0530

    changes message for captcha matched

--- Captcha.cpp
+++ Captcha.cpp
@@ -24,7 +24,7 @@ int main()
     cout<<"Enter Captcha: "<<endl;
     cin>> usr_captcha;
     if(CheckCaptcha(captcha, usr_captcha))
-        cout<<"Captcha Matched"<<endl;
+        cout<<"Captcha Matched hogya"<<endl;
     else
         cout<<"Captcha Not Matched"<<endl;
 
